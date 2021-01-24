e940f9cee5a8365cc2ce1987c244c8dabf333a56
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-24 16:25:02 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Fulcherberguer, Fernanda on 2021-01-24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
